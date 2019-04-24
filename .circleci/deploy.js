27151904633b02700cbd0f5ad256ec15189a12c4
misja.loeffen <misja.loeffen@rabobank.nl>
2019-04-24 11:10:04 +0200

    firstDeploy5

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -6,7 +6,7 @@ var config = {
     password: process.env.FTPPASS,
     host: process.env.FTPHOST,
     port: 21,
-    localRoot: "C:/Projects/responsivePortfolio/portfolio",
+    localRoot: "C:\Projects\responsivePortfolio\portfolio",
     remoteRoot: "/public_html/",
     include: ['*']
 }
